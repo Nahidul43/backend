0d75b1671fdd3949d1bd8f6a6f59d458be7ac23b
Nahidul43 <nahidulislamjihad43@gmail.com>
2026-07-26 21:58:23 +0600

    fix allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -13,9 +13,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-change-this-in-production')
 DEBUG = config('DEBUG', default=True, cast=bool)
 ALLOWED_HOSTS = config(
-    'ALLOWED_HOSTS',
-    default='localhost,127.0.0.1,.onrender.com'
-).split(',')
+    "ALLOWED_HOSTS",
+    default="localhost,127.0.0.1,backend-qsu7.onrender.com"
+).split(",")
 # Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
